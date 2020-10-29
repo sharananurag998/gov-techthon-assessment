98104fe8d93e9e72d2178fa8880dc4758e44a3d4
Anurag Sharan <sharananurag998@gmail.com>
2020-10-29 14:44:54 +0530

    Added phone verification

--- src/components/firebase/index.js
+++ src/components/firebase/index.js
@@ -14,4 +14,4 @@ const firebaseApp = firebase.initializeApp({
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
 
-export { db, auth };
+export { db, auth, firebase };
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
+import {useHistory} from 'react-router-dom';
 import './Login.css'
-
+import {auth, firebase} from '../firebase';
 function Login() {
+    const history = useHistory();
+    const [mobile, setMobile] = useState("");
+    const [OTPsent, setOTPsent] = useState(false);
+    const [otp, setOtp] = useState(null);
+    const [user, setUser] = useState(null);
+    const [confirm, setConfirm] = useState(null);
+    const signIn = (e) => {
+        e.preventDefault();
+        var appVerifier = window.recaptchaVerifier;
+        auth.signInWithPhoneNumber(mobile, appVerifier)
+        .then(function (confirmationResult) {
+            setOTPsent(true);
+            setConfirm(confirmationResult);
+        // SMS sent. Prompt user to type the code from the message, then sign the
+        // user in with confirmationResult.confirm(code).
+        window.confirmationResult = confirmationResult;
+        }).catch(function (error) {
+            console.error(error);
+        });
+
+    };
+    useEffect(() => {
+        window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('sign-in-button', {
+        'size': 'invisible',
+        'callback': function(response) {
+            // reCAPTCHA solved, allow signInWithPhoneNumber.
+            signIn();
+        }
+        });
+    }, [])
+
+    const finalSignIn = (e) => {
+        e.preventDefault();
+            confirm.confirm(otp).then(function (result) {
+            setUser(result.user);
+            console.log(user);
+            history.push({pathname:"/"})
+            }).catch(function (error) {
+                console.error(error);
+            });
+    }
     return (
         <div className="container">
             <div className="login__header">
@@ -11,10 +53,11 @@ function Login() {
                 <div className="login">
                     <h1 className="login__heading">Jury Login</h1>
                     <form >
-                        <label htmlFor="mobNumber">Mobile Number</label>
-                        <input type="tel" name="mobNumber" id="mobNumber" placeholder="Enter your Mobile Number" required/>
+                        <label htmlFor="mobNumber">Mobile Number (Precede with country code(+91))</label>
+                        <input type="tel" name="mobNumber" id="mobNumber" placeholder="Enter your Mobile Number" onChange={(e)=>{setMobile(e.target.value)}} required/>
+                        {OTPsent&&<input type="number" name="otp" id="otp" placeholder="Enter OTP" onChange={(e)=>{setOtp(e.target.value)}} required/>}
                         <br/>
-                        <button type="submit" className="login__button">Login</button>
+                        <button onClick={OTPsent?(e)=>finalSignIn(e):(e)=>signIn(e)} id="sign-in-button" className="login__button">{OTPsent?"Login":"Send OTP"}</button>
                     </form>
                 </div>
             </div>

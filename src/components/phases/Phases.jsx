91790922ee9f999ef7f9ded8f26f0f4ebc8423a7
Anurag Sharan <sharananurag998@gmail.com>
2020-10-29 15:59:34 +0530

    Added user details in phase screen

--- src/components/phases/Phases.jsx
+++ src/components/phases/Phases.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import './Phases.css';
+import {db, firebase} from '../firebase';
 
-function Phases() {
+function Phases({user}) {
+    const [name, setName]= useState(null);
+    const [email, setEmail] = useState(null);
+    const [phone, setPhone] = useState(null)
+    const [details, setDetails] = useState(null);
+    useEffect(async() => {
+        const usr = firebase.auth().currentUser;
+        const snapshot = await db.collection("jury").get();
+        setDetails(snapshot.docs.reduce(function (acc, doc, i) {
+              acc[doc.id] = doc.data();
+              return acc;
+            }, {}));
+        if(details){
+            Object.keys(details).map(function(key, index) {
+            if(details[key].phone == usr.phoneNumber)
+            {
+                setName(details[key].name);
+                setPhone(details[key].phone);
+                setEmail(details[key].email);
+                console.log("User found in db");
+            }else{
+                console.log("User not in db");
+            }
+        });
+        }
+
+    }, [details])
+    useEffect(() => {
+    }, [])
     return (
         <div className="phases__container">
-
+            <p>Name: {name?name:"Jury Not in DB"}</p>
+            <p>Email: {email}</p>
+            <p>Phone: {phone}</p>
             <h2 className="phases__heading">Select the Phase to Evaluate</h2>
 
             <p className="phases__name">
